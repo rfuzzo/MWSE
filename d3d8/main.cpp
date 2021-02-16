f44de55cf9a647b2c1cb6bac09879a065bd9bc1a
NullCascade <nullcascade@gmail.com>
2021-02-16 12:44:39 -0800

    Load MWSE D3D/DI functions on initial attachment, rather than later on.

--- d3d8/main.cpp
+++ d3d8/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
-static HMODULE mwseDLL = 0;
+typedef void* (_stdcall* Direct3DCreate8_t) (UINT);
+static Direct3DCreate8_t MWSE_Direct3DCreate8 = nullptr;
 
 extern "C" BOOL _stdcall DllMain(HANDLE hModule, DWORD reason, void* unused) {
     if (reason != DLL_PROCESS_ATTACH) {
@@ -8,11 +9,16 @@ extern "C" BOOL _stdcall DllMain(HANDLE hModule, DWORD reason, void* unused) {
     }
 
     // Ensure that we attach mwse.dll immediately. Using the entry point isn't good enough.
-    mwseDLL = LoadLibraryA("mwse.dll");
+    auto MWSE = LoadLibraryA("mwse.dll");
+    if (!MWSE) {
+        return false;
+    }
+
+    MWSE_Direct3DCreate8 = Direct3DCreate8_t(GetProcAddress(MWSE, "Direct3DCreate8"));
+
     return true;
 }
 
-typedef void* (_stdcall* Direct3DCreate8_t) (UINT);
 extern "C" void* __stdcall FakeDirect3DCreate8(UINT version) {
-    return Direct3DCreate8_t(GetProcAddress(mwseDLL, "Direct3DCreate8"))(version);
+    return MWSE_Direct3DCreate8(version);
 }
--- dinput8/main.cpp
+++ dinput8/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
-static HMODULE mwseDLL = 0;
+typedef HRESULT(__stdcall* DirectInput8Create_t)(HINSTANCE, DWORD, REFIID, LPVOID*, void*);
+static DirectInput8Create_t MWSE_DirectInput8Create = nullptr;
 
 extern "C" BOOL _stdcall DllMain(HANDLE hModule, DWORD reason, void* unused) {
     if (reason != DLL_PROCESS_ATTACH) {
@@ -8,11 +9,16 @@ extern "C" BOOL _stdcall DllMain(HANDLE hModule, DWORD reason, void* unused) {
     }
 
     // Ensure that we attach mwse.dll immediately. Using the entry point isn't good enough.
-    mwseDLL = LoadLibraryA("mwse.dll");
+    auto MWSE = LoadLibraryA("mwse.dll");
+    if (!MWSE) {
+        return false;
+    }
+
+    MWSE_DirectInput8Create = DirectInput8Create_t(GetProcAddress(MWSE, "DirectInput8Create"));
+
     return true;
 }
 
-typedef HRESULT(__stdcall* DirectInput8Create_t)(HINSTANCE, DWORD, REFIID, LPVOID*, void*);
 extern "C" HRESULT __stdcall FakeDirectInput8Create(HINSTANCE a, DWORD b, REFIID c, void** d, void* e) {
-    return DirectInput8Create_t(GetProcAddress(mwseDLL, "DirectInput8Create"))(a, b, c, d, e);
+    return MWSE_DirectInput8Create(a, b, c, d, e);
 }
