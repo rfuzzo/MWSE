f1f9537e88aafa6ed090beb0697ec7d242191017
C3pa <41503714+C3pa@users.noreply.github.com>
2023-07-04 17:26:49 +0200

    Docs: add `attr_list` markdown extension needed for the buttons
    Adjust the button css to look good
    
    Remove unneeded eventData from the simulated event definition.

--- autocomplete/definitions/events/standard/simulated.lua
+++ autocomplete/definitions/events/standard/simulated.lua
@@ -1,6 +1,4 @@
 return {
 	type = "event",
 	description = "The simulated event occurs after almost all game logic, excluding when the game is paused or in menu mode. This event is intended to allow changes to the scene that will not be overwritten by game logic.",
-	eventData = {
-	},
 }
--- docs/source/events/simulated.md
+++ docs/source/events/simulated.md
@@ -18,6 +18,3 @@ event.register(tes3.event.simulated, simulatedCallback)
 !!! tip
 	An event can be claimed by setting `e.claim` to `true`, or by returning `false` from the callback. Claiming the event prevents any lower priority callbacks from being called.
 
-## Event Data
-
-
