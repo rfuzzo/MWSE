af483f2a09f676474d940f47c2b77480c0a29ace
Hrnchamd <iamalannoble@hotmail.com>
2023-07-13 03:19:42 +0100

    Add `tes3light.createCopy`.

--- misc/package/Data Files/MWSE/core/meta/class/tes3light.lua
+++ misc/package/Data Files/MWSE/core/meta/class/tes3light.lua
@@ -25,6 +25,23 @@
 --- @field weight number The weight of the object.
 tes3light = {}
 
+--- Creates a copy of this object.
+--- @param params tes3light.createCopy.params This table accepts the following values:
+--- 
+--- `id`: string? — *Optional*. The new object's ID. If one is not provided, a randomly generated one will be used.
+--- 
+--- `addToObjectList`: boolean? — *Default*: `true`. If true, the object will be added to the data handler. If this is false, the new object may not have a randomly generated ID. Do not use this without knowing the implications.
+--- 
+--- `sourceless`: boolean? — *Default*: `false`. If true, the object will be made sourceless, and will not be serialized to the save game. If the object is copied outside of a save game, the object will **always** be sourceless.
+--- @return tes3light newObject No description yet available.
+function tes3light:createCopy(params) end
+
+---Table parameter definitions for `tes3light.createCopy`.
+--- @class tes3light.createCopy.params
+--- @field id string? *Optional*. The new object's ID. If one is not provided, a randomly generated one will be used.
+--- @field addToObjectList boolean? *Default*: `true`. If true, the object will be added to the data handler. If this is false, the new object may not have a randomly generated ID. Do not use this without knowing the implications.
+--- @field sourceless boolean? *Default*: `false`. If true, the object will be made sourceless, and will not be serialized to the save game. If the object is copied outside of a save game, the object will **always** be sourceless.
+
 --- Gets the time remaining for a light, given a `tes3itemData`, `tes3reference`, or `tes3equipmentStack`.
 --- @param data tes3reference|tes3itemData|tes3equipmentStack No description yet available.
 --- @return number result No description yet available.
