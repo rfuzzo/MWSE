80e1962d0d3ea2edf6907da7370a5932c3573a46
C3pa <41503714+C3pa@users.noreply.github.com>
2023-09-16 19:29:12 +0200

    Docs: add MCM infos

---
+++ autocomplete/definitions/namedTypes/mwseMCMInfo/makeComponent.lua
@@ -0,0 +1,7 @@
+return {
+	type = "method",
+	description = [[This method creates the info (a label) UI element of the mwseMCMInfo and stores it in `self.elements.info` and `self.mouseOvers`.]],
+	arguments = {
+		{ name = "parentBlock", type = "tes3uiElement" }
+	}
+}
---
+++ autocomplete/definitions/namedTypes/mwseMCMInfo/text.lua
@@ -0,0 +1,5 @@
+return {
+	type = "value",
+	description = [[The current text of the Info.]],
+	valuetype = "string",
+}
