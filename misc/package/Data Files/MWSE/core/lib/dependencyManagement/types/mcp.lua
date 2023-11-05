10b463804bdb3e9269b869db124be164094a532b
C3pa <41503714+C3pa@users.noreply.github.com>
2023-11-05 12:39:30 +0100

    Dependency Management mcp: improve error messages
    Use patch names from the `mcpatch\\describe.json` if it's present

--- misc/package/Data Files/MWSE/core/lib/dependencyManagement/types/mcp.lua
+++ misc/package/Data Files/MWSE/core/lib/dependencyManagement/types/mcp.lua
@@ -4,19 +4,42 @@
 
 
 --- Inspired by this thread: https://lua-users.org/lists/lua-l/2006-01/msg00231.html
----@param enum string
-local function getFeatureName(enum)
+---@param enumName string
+local function splitCamelCase(enumName)
 	-- Make the first letter uppercase
-	enum = enum:gsub("^%l", string.upper)
+	enumName = enumName:gsub("^%l", string.upper)
 	-- Insert space between lowercase and uppercase character
-	enum = enum:gsub( "(%l)(%u)", "%1 %2" )
-	return enum
+	enumName = enumName:gsub( "(%l)(%u)", "%1 %2" )
+	return enumName
 end
+
+---@param enum string
+---@param descriptions table|nil
+local function getFeatureName(enum, descriptions)
+	if not descriptions then
+		return splitCamelCase(enum)
+	end
+
+	local id = tes3.codePatchFeature[enum]
+	for _, category in ipairs(descriptions) do
+		for _, patch in ipairs(category.patches) do
+			if patch.id == id then
+				return string.format("%s (%s)", patch.brief, category.category)
+			end
+		end
+	end
+
+	-- If the given feature isn't present in the describe.json fall back to the generalized solution
+	return splitCamelCase(enum)
+end
+
+local mcpDescribePath = "..\\..\\mcpatch\\describe.json"
 
 ---@param dependencyManager DependencyManager
 ---@param dependency MWSE.Metadata.Dependency.MCP
 local function check(dependencyManager, dependency)
 	local missingFeatures = {}
+	local descriptions = json.loadfile(mcpDescribePath)
 
 	for feature, needed in pairs(dependency.features) do
 		local id = tes3.codePatchFeature[feature]
@@ -26,7 +49,7 @@ local function check(dependencyManager, dependency)
 			local hasFeature = tes3.hasCodePatchFeature(id)
 			if hasFeature ~= needed then
 				local message = needed and [["%s" needs to be enabled.]] or [["%s" needs to be disabled.]]
-				missingFeatures[#missingFeatures + 1] = string.format(message, getFeatureName(feature))
+				missingFeatures[#missingFeatures + 1] = string.format(message, getFeatureName(feature, descriptions))
 			end
 		end
 	end
