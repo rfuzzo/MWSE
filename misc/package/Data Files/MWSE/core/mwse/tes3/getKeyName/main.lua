bd3f6babf718b469a956f02d16880c061125c4f6
herbert100 <154224164+herbertgithubaccount@users.noreply.github.com>
2024-02-20 09:06:25 +0100

    streamlined the code a bit

--- misc/package/Data Files/MWSE/core/mwse/tes3/getKeyName/main.lua
+++ misc/package/Data Files/MWSE/core/mwse/tes3/getKeyName/main.lua
@@ -2,11 +2,12 @@
 function tes3.getKeyName(keyCode)
     -- get the index of the appropriate GMST setting
     -- the `%02X` does hexadecimal formatting and gives numbers `< 16` a leading 0.
-    -- so, `11` -> "0A" and `16` -> "10"
-    local index = string.format("sKeyName_%02X", keyCode)
-	local gmst = tes3.gmst[index]
+    --      so, `11` -> "0A" and `16` -> "10"
+    -- for example, if `keyCode == tes3.scanCode["2"] == 3`, then
+    --      `string.format("sKeyName_%02X", keyCode) == sKeyName_03`
+	local index = tes3.gmst[string.format("sKeyName_%02X", keyCode)]
     -- if the game setting exists, return its value
-	if gmst then
-		return tes3.findGMST(gmst).value
+	if index then
+		return tes3.findGMST(index).value
 	end
 end
