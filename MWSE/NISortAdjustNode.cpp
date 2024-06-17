da435e4c510a35ed7594cc843369175003516d92
Hrnchamd <iamalannoble@hotmail.com>
2024-06-17 01:26:20 +0100

    Fix niSortAdjustNode lua constructor.

--- MWSE/NISortAdjustNode.cpp
+++ MWSE/NISortAdjustNode.cpp
@@ -1,7 +1,9 @@
 #include "NISortAdjustNode.h"
 
 namespace NI {
+	const auto SortAdjustNode_vTable = reinterpret_cast<Node_vTable*>(0x750580);
 	SortAdjustNode::SortAdjustNode() : Node() {
+		vTable.asNode = SortAdjustNode_vTable;
 		sortingMode = SortAdjustMode::SORTING_INHERIT;
 	}
 
