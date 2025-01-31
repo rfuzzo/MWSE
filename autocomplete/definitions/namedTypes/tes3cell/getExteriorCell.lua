2738fb6ac6b6b798640614563cd8043d3dfae540
NullCascade <nullcascade@gmail.com>
2025-01-31 15:39:09 -0800

    Revert "Add tes3cell:getExteriorCell, which searches through load doors for an exterior."
    This reverts commit dcd012391acb7fc315839bce789dbee22f2037ea.

--- autocomplete/definitions/namedTypes/tes3cell/getExteriorCell.lua
+++
@@ -1,7 +0,0 @@
-return {
-	type = "method",
-	description = [[Gets an exterior associated with this cell. If the cell is or behaves as an exterior, it returns the current cell. Otherwise it looks through load doors, searching for an exterior cell. If the cell links to multiple exteriors, there is no guarantee that the returned exterior is the closest one to the player.]],
-	returns = {
-		{ name = "exterior", type = "tes3cell?", description = "An exterior cell linked to this cell." },
-	}
-}
